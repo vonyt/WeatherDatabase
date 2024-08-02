ce442193c3aeaf5d133424ca35dc72e4b32ffd03
Al <aharvin105@gmail.com>
2024-08-01 21:03:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request, render_template
 import requests
 
 app = Flask(__name__)
-API_KEY = 'your-api-key'
+API_KEY = 'f7f9afd4ce42103863ceed10f3278e0c'
 
-cities=[{"name": "Austin", "id":4671654},{"name": "San Diego", "id": 5392171}] #'Houston', 'Dallas', 'New York', 'Palo Alto', 'Sacramento', 'Georgetown','Manchester','West Haven']
+cities=[{"name": "Austin", "id":4671654},{"name": "Chicago", "id":4887398},{"name": "San Jose", "id": 5392171}, {"name": "Houston", "id": 4697616}, {"name": "Dallas", "id": 4684888},{"name": "New York", "id": 5128581}, {"name": "Palo Alto", "id": 5392171}, {"name": "Sacramento", "id": 5392171},{"name": "Miami", "id": 4164138}, {"name": "Albuquerque", "id":5454711}]
 
 @app.route('/')
 def home():
@@ -28,7 +28,9 @@ def currWeather():
 	cityWeather = weatherInCity(userCity['id'])
 	if cityWeather is None:
 		return "Weather data could not be found"
-	return render_template('currWeather.html', weather=cityWeather)
+	weatherid = cityWeather['weather'][0]['icon']
+	weatherIcon=f"http://openweathermap.org/img/wn/{weatherid}@2x.png"
+	return render_template('currWeather.html', weather=cityWeather, weatherIcon=weatherIcon)
 
 
 if __name__ == '__main__':
